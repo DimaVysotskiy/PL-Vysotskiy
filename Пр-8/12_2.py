bcdafc3922caed8119f4ff7a2231f12465f7d015
DimaVysotskiy <dimavysockij258@gmail.com>
2024-10-26 15:30:48 +0300

    rework 12_2.py, for situation if user enter only one row and situation if input < 0

--- Пр-8/12_2.py
+++ Пр-8/12_2.py
@@ -7,41 +7,49 @@ while flag == False:
         m = int(input('Enter the quantity of items in row:'))
         print()
 
-        #generation matrix
-        a = []
-        for i in range(n):
-            b = []
-            for j in range(m):
-                #input in matrix
-                x = int(input(f'Enter item from matrix in position[{i}][{j}]:'))
-                b.append(x)
-            a.append(b)
-        print('~' * m * 2)
-
-        #matrix output(unprocessed)
-        for row in a:
-            print(*row)
-        print('~' * m * 2)
-
-        #matrix processing
-        for i in range(n - 1):
-            for j in range(m):
-                a[i][j] -= a[n - 1][j]
-
-        # matrix output(processed)
-        for row in a:
-            print(*row)
-        print('~' * m * 2)
-
-        #for try-except block
-        flag = True
-        print('End')
-
-        #if enter only one row
+        # if enter only one row
         if n == 1:
             print('Enter more than one row. This operation meaningless.')
+            print()
+
+        # if input < 0
+        elif n <= 0 or m <= 0:
+            print('Enter value > 0')
+            print()
+
+        else:
+            #generation matrix
+            a = []
+            for i in range(n):
+                b = []
+                for j in range(m):
+                    #input in matrix
+                    x = int(input(f'Enter item from matrix in position[{i}][{j}]:'))
+                    b.append(x)
+                a.append(b)
+            print('~' * m * 2)
+
+            #matrix output(unprocessed)
+            for row in a:
+                print(*row)
+            print('~' * m * 2)
+
+            #matrix processing
+            for i in range(n - 1):
+                for j in range(m):
+                    a[i][j] -= a[n - 1][j]
+
+            # matrix output(processed)
+            for row in a:
+                print(*row)
+            print('~' * m * 2)
+
+            #for try-except block
+            flag = True
+            print('End')
 
     except ValueError:
+        print()
         print('Input error! Enter only integers!', )
         print('/////////////////////////////////')
         print()

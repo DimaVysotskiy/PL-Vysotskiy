ffea3e8f749fc00f92539dcebe078f01de4ba73e
DimaVysotskiy <dimavysockij258@gmail.com>
2024-10-26 15:41:31 +0300

    rework 12_1.py,add condition for entering negative numbers

--- Пр-8/12_1.py
+++ Пр-8/12_1.py
@@ -7,35 +7,41 @@ while flag == False:
         k1 = int(input('Enter the quantity of row:'))
         print()
 
-        #matrix generation
-        a = []
-        for i in range(k1):
-            b = ['*'] * k
-            a.append(b)
+        # if input < 0
+        if k <= 0 or k1 <= 0:
+            print('Enter value > 0')
+            print()
+        else:
+            #matrix generation
+            a = []
+            for i in range(k1):
+                b = ['*'] * k
+                a.append(b)
 
-        #matrix output(unprocessed)
-        for row in a:
-            print(*row)
-        print()
+            #matrix output(unprocessed)
+            for row in a:
+                print(*row)
+            print()
 
-        #processing matrix
-        for i in range(k1):
-            for j in range(k):
-                if i == j:
-                    a[i][j] = '+'
-                else:
-                    a[i][j] = '-'
+            #processing matrix
+            for i in range(k1):
+                for j in range(k):
+                    if i == j:
+                        a[i][j] = '+'
+                    else:
+                        a[i][j] = '-'
 
-        #matrix output(processed)
-        for row in a:
-            print(*row)
-        print()
+            #matrix output(processed)
+            for row in a:
+                print(*row)
+            print()
 
-        # for try-except block
-        flag = True
-        print('End')
+            # for try-except block
+            flag = True
+            print('End')
 
     except ValueError:
-        print('Input error! Enter only integers!', )
+        print()
+        print('Input error! Enter only integers!')
         print('/////////////////////////////////')
         print()

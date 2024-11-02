bff303930039c112a4a83a0a869857d0762a30d1
DimaVysotskiy <dimavysockij258@gmail.com>
2024-11-02 18:13:42 +0300

    rework block_B_1.py

--- Пр-9/block_B_1.py
+++ Пр-9/block_B_1.py
@@ -1,21 +1,31 @@
-#processing output list from def f()
-def f1(a):
-    s = []
-    while type(a[-1]) is list:
-        s.append(a[0])
-        a = a[-1]
-    s.append(a[0])
-    return print(max(s))
-
-#def for input
 def f():
     n = int(input())
     if n == 0:
         return 0
     else:
-        return [n, f()]
+        return max([n, f()])
 
-print(f1(f()))
+print(f())
+
+
+# #processing output list from def f()
+# def f1(a):
+#     s = []
+#     while type(a[-1]) is list:
+#         s.append(a[0])
+#         a = a[-1]
+#     s.append(a[0])
+#     return print(max(s))
+#
+# #def for input
+# def f():
+#     n = int(input())
+#     if n == 0:
+#         return 0
+#     else:
+#         return [n, f()]
+#
+# print(f1(f()))
 
 
 

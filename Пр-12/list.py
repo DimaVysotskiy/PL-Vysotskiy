0a74d81418ad4a3f277a46803bc7b31f5eafc72f
DimaVysotskiy <dimavysockij258@gmail.com>
2024-11-10 02:59:28 +0300

    Пр-12 added comments

--- Пр-12/list.py
+++ Пр-12/list.py
@@ -1,47 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+
+
 url = 'https://habr.com/ru/post/453444/'
 get = requests.get(url=url)
-
-soup = BeautifulSoup(get.text, 'html.parser')
-
+soup = BeautifulSoup(get.text, 'html.parser')#нормальный вид html + find_all
 
 
-#список с именами репозиториев
-soup_name = soup.find_all('b')
 
-list_with_name_of_repository = []
+
+#список с именами проектов
+soup_name = soup.find_all('b')#'b' - tag name, soup_name - список, но заполненный не str() типами данных
+
+names_of_projects = []
 for i in range(len(soup_name)):
     a = soup_name[i].text
     if i == 0:
         a = a.replace('1. ', '')#кривая запись первого имени в самой статье
-        list_with_name_of_repository.append(a)
+        names_of_projects.append(a)
     else:
-        list_with_name_of_repository.append(a)
+        names_of_projects.append(a)
 
 
 
 #список с именами на github
 soup_reference = soup.find_all('a')
 
-name = []
+names_on_git = []
 for i in range(len(soup_reference)):
     a = soup_reference[i].text
     if 'github.com' in a:
-        a = a.split('/')[-1]
-        name.append(a)
+        a = a.split('/')[-1]#конец ссылки --> username in GitHub
+        names_on_git.append(a)
 
 
 
 
 
-
-#словарь имя --> ссылка
+#словарь {'имя проекта': 'имя на GitHub'}
 final_dict = {}
 for i in range(100):
-    final_dict[list_with_name_of_repository[i]] = name[i]
+    final_dict[names_of_projects[i]] = names_on_git[i]
 
 
 with open('list_txt', '+w') as file:
-    file.write(f'{str(final_dict)}\n{str(name)}')
+    file.write(f'{str(final_dict)}\n{str(names_on_git)}')
